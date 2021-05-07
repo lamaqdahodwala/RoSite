51612e6034b2e6812a6f021854acae2033f9f137
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-07 17:31:12 -0400

    hooray

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('redirect', views.redirection, name='redirection'), 
+    path('userid/<int:uid>', views.userid, name='redirection')
 ]
