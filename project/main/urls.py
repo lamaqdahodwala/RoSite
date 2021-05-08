57c733a5744960a6dc28f43576db3949791c94c8
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-08 11:31:53 -0400

    f

--- project/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('redirect', views.redirection, name='redirection'), 
-    path('userid/<int:uid>', views.userid, name='redirection')
-]
